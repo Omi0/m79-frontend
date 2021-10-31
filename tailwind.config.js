45bf94c910b9c8a977ec25ae619c7ef8c52c47c8
Timofey Sergeev <timofey.sergeev@verivox.com>
2021-10-31 18:01:39 +0100

    add fontawesome, autoprefixer, postcss, tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  mode: 'jit',
+  prefix: 'tw-',
+  purge: {
+    enabled: true,
+    content: ["./src/**/*.{html,ts}"],
+  },
+  darkMode: "class",
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  }
+};
